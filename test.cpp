3c974148bb3523600ee4bb31f2e9a77270765dcc
AudibleP <64707877+AudibleP@users.noreply.github.com>
2021-10-05 14:15:46 -0500

    Agrega LinkedList
    -MergeSort
    +LinkedList
    +Show()
    +DeleteAt
    +Insertion()

--- test.cpp
+++ test.cpp
@@ -7,9 +7,12 @@ nombres de los juegos en orden alfabetico.
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <list>
 #include <iterator>
+#include <string>
 
 #include "sorts.h"
+#include "lists.h"
 
 using namespace std;
 
@@ -17,43 +20,68 @@ using namespace std;
 void menu(){
     cout<<"Elige una opcion: "<<"\n";
 
-    cout<<"1. Mostrar el archivo sin ordenar."<<"\n";
+    cout<<"1. Carga el archivo."<<"\n";
     cout<<"2. Mostrar el archivo ordenado."<<"\n";
-    cout<<"3. Salir."<<"\n";
+    cout<<"3. Agregar nombre a la lista."<<"\n";
+    cout<<"4. Borrar nombre de la lista."<<"\n";
+    cout<<"5. Ve la lista."<<"\n";
+    cout<<"6. Salir."<<"\n";
 }
 
 int main(){
     int opcion = 0;
-    while(opcion>-1 && opcion<6){
+    List<string>v;
+    string str, ListData;
+    ifstream file ("Steam.csv");
+    while(opcion>-1 && opcion<9){
         menu();
-
-        ifstream file ("Steam.csv");
         cin>>opcion;
         if(opcion ==1){
-            string name, hour;
-            while(file.good()){
-                getline(file, name, ',');
+            //string name, hour;
+            while(getline(file,ListData)){
+                if(ListData.size() > 0){
+                    v.insertion(ListData);
+                }
+            }
+            //while(file.good()){
+                /*getline(file, name, ',');
                 getline(file, hour, '\n');
                 cout << "Name: "<<name<<'\n';
                 cout << "Time Played: "<<hour<<" Hours"<<'\n'<<"\n";
-            }
+            }*/
         file.close();
         }
 
         else if(opcion == 2){
             Sorts <string> sorts;
+            cout<<"Benji pls fix me\n";
 
-            vector<string>v;
-            string str;
             while(file >> str){
-                v.push_back(str);
+                v.insertion(str);
+                v.toString();
             }
-            sorts.ordenaMerge(v);
-            copy (v.begin(),v.end(),ostream_iterator<string>(cout,"\n\n"));
-            cout << v.size();
+            //sorts.ordenaMerge(v);
+            //copy (v.begin(),v.end(),ostream_iterator<string>(cout,"\n\n"));
+            //cout << v.size();
         }
 
         else if(opcion == 3){
+            cout<<"Agrega nombre \n\n";
+            cin.ignore();
+            getline(cin,ListData);
+            v.insertion(ListData);
+        }
+        else if(opcion == 4){
+            cout<<"Agrega nombre para eliminar \n\n";
+            cin.ignore();
+            getline(cin,ListData);
+            v.deleteAt(ListData);
+        }
+        else if(opcion == 5){
+            v.show();
+            cout<<"\n";
+        }
+        else if(opcion == 6){
             cout<<"bye \n\n";
             return 0;
         }
