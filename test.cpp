2e4d20f128af70ab8bab06e7cd2a34e7df9cef60
AudibleP <64707877+AudibleP@users.noreply.github.com>
2021-10-31 21:54:18 -0600

    bst y merge

--- test.cpp
+++ test.cpp
@@ -7,12 +7,10 @@ nombres de los juegos en orden alfabetico.
 #include <iostream>
 #include <fstream>
 #include <vector>
-#include <list>
 #include <iterator>
-#include <string>
-
+#include <sstream>
 #include "sorts.h"
-#include "lists.h"
+#include "bst.h"
 
 using namespace std;
 
@@ -20,68 +18,122 @@ using namespace std;
 void menu(){
     cout<<"Elige una opcion: "<<"\n";
 
-    cout<<"1. Carga el archivo."<<"\n";
+    cout<<"1. Mostrar el archivo sin ordenar."<<"\n";
     cout<<"2. Mostrar el archivo ordenado."<<"\n";
-    cout<<"3. Agregar nombre a la lista."<<"\n";
-    cout<<"4. Borrar nombre de la lista."<<"\n";
-    cout<<"5. Ve la lista."<<"\n";
-    cout<<"6. Salir."<<"\n";
+    cout<<"3. Arboles."<<"\n";
+    cout<<"4. Salir."<<"\n";
 }
 
 int main(){
     int opcion = 0;
-    List<string>v;
-    string str, ListData;
-    ifstream file ("Steam.csv");
-    while(opcion>-1 && opcion<9){
+    while(opcion>-1 && opcion<6){
         menu();
+        string buffer;
+        string temporary;
+        vector <pair <string, int> >  patVecName;
+        vector <pair <int, string> >  patVecHour;
+        pair <string, int> patPairName;
+        pair <int, string> patPairHour;
+        int plength;
+        string pCharacters;
+
+        ifstream file ("steam.csv");
         cin>>opcion;
         if(opcion ==1){
-            //string name, hour;
-            while(getline(file,ListData)){
-                if(ListData.size() > 0){
-                    v.insertion(ListData);
-                }
+
+            while(file.good()){
+
+                getline(file, buffer,',');
+                stringstream str_reader(buffer);
+                str_reader >> pCharacters;
+
+
+                getline(file, buffer);
+                stringstream int_reader(buffer);
+                int_reader >> plength;
+
+
+                patPairName = make_pair (pCharacters,plength);
+                cout << "Game name: " << pCharacters <<"---> Play time: "<< plength << " hours."<<endl <<endl;
+
             }
-            //while(file.good()){
-                /*getline(file, name, ',');
-                getline(file, hour, '\n');
-                cout << "Name: "<<name<<'\n';
-                cout << "Time Played: "<<hour<<" Hours"<<'\n'<<"\n";
-            }*/
-        file.close();
+            file.close();
         }
 
         else if(opcion == 2){
-            Sorts <string> sorts;
-            cout<<"Benji pls fix me\n";
+            int tarde;
+            cout<<"1-Ordena por nombre ///// 2-Ordena por horas"<<endl;
+            cin>>tarde;
+            if(tarde ==1){
+                Sorts <pair<string,int>> sorts;
+                while(file.good()){
+                    getline(file, buffer,',');
+                    stringstream str_reader(buffer);
+                    str_reader >> pCharacters;
 
-            while(file >> str){
-                v.insertion(str);
-                v.toString();
+
+                    getline(file, buffer);
+                    stringstream int_reader(buffer);
+                    int_reader >> plength;
+
+
+                    patPairName = make_pair (pCharacters,plength);
+                    patVecName.push_back(patPairName);
+                }
+                sorts.ordenaMerge(patVecName);
+                for(int i=0; i<patVecName.size(); i++){
+                    cout << "Game name: " << patVecName[i].first <<"---> Play time: "<< patVecName[i].second << " hours."<<endl <<endl;
+                }
             }
-            //sorts.ordenaMerge(v);
-            //copy (v.begin(),v.end(),ostream_iterator<string>(cout,"\n\n"));
-            //cout << v.size();
+            else if(tarde == 2){
+                Sorts <pair<int,string>> sorts;
+                while(file.good()){
+                    getline(file, buffer,',');
+                    stringstream str_reader(buffer);
+                    str_reader >> pCharacters;
+
+
+                    getline(file, buffer);
+                    stringstream int_reader(buffer);
+                    int_reader >> plength;
+
+
+                    patPairHour = make_pair (plength,pCharacters);
+                    patVecHour.push_back(patPairHour);
+                }
+                sorts.ordenaMerge(patVecHour);
+                for(int i=0; i<patVecHour.size(); i++){
+                    cout << "Play time: " << patVecHour[i].first <<" hours. ---> Game name: "<< patVecHour[i].second <<endl <<endl;
+                }
+            }
+            else{
+                cout<<"Opcion Invalida \n\n";
+            }
+
         }
 
         else if(opcion == 3){
-            cout<<"Agrega nombre \n\n";
-            cin.ignore();
-            getline(cin,ListData);
-            v.insertion(ListData);
+
+            BST<int>bst;
+            while(file.good()){
+                    getline(file, buffer,',');
+                    stringstream str_reader(buffer);
+                    str_reader >> pCharacters;
+
+
+                    getline(file, buffer);
+                    stringstream int_reader(buffer);
+                    int_reader >> plength;
+
+
+                    patPairHour = make_pair (plength,pCharacters);
+                    bst.add(patPairHour.first);
+                }
+            cout<<"PreOrder"<<endl;cout<<"InOrder"<<endl;cout<<"PostOrder"<<endl;cout<<"LevelByLevel"<<endl;
+            cout<<bst.visit();
         }
+
         else if(opcion == 4){
-            cout<<"Agrega nombre para eliminar \n\n";
-            cin.ignore();
-            getline(cin,ListData);
-            v.deleteAt(ListData);
-        }
-        else if(opcion == 5){
-            v.show();
-            cout<<"\n";
-        }
-        else if(opcion == 6){
             cout<<"bye \n\n";
             return 0;
         }
